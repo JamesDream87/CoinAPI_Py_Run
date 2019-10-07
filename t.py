79207174c6e4a41b51c739ea127d47d4366335c7
JamesDream <1741704351@qq.com>
2019-10-07 17:28:35 +0800

    update

--- t.py
+++ t.py
@@ -1,9 +1,22 @@
 import datetime
 
 def main(year, month):
+
+  # if year == 2019 and month == 9:
+  #   print('end....')
+  # else:
+  if(month == 12):
+    newYear = year + 1
+    newMonth = 1
+  else:
+    newYear = year
+    newMonth = month + 1
+
   a = datetime.datetime(year, month, 13, 16, 00, 00)
+  b = datetime.datetime(newYear, newMonth, 13, 16, 00, 00)
   a = datetime.datetime.strftime(a,'%Y-%m-%dT%H:%M:%S')
-  print(a)
+  b = datetime.datetime.strftime(b,'%Y-%m-%dT%H:%M:%S')
+  print(a,'-------',b)
 
 def count():
   year = 2011
@@ -17,7 +30,7 @@ def count():
     else:
       month += 1
     
-    if year == 2019 and month == 10 :
+    if year == 2019 and month == 9 :
       break
     
     main(year,month)
